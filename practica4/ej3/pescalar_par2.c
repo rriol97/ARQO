6e88bc570b9290538cf360de2d66b18b54b06433
RRiol <ricardo.riol97@gmail.com>
2017-12-06 01:42:35 +0100

    Vaya pedazo de script me he marcado para ej ej 2

---
+++ practica4/ej3/pescalar_par2.c
@@ -0,0 +1,53 @@
+// ----------- Arqo P4-----------------------
+// pescalar_par2
+//
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "arqo4.h"
+
+int main(int argc, char *argv[]){
+	int thr, tam;
+	float *A=NULL, *B=NULL;
+	long long k=0;
+	struct timeval fin,ini;
+	float sum=0;
+
+	if (argc != 3){
+		printf ("Error en los argumentos de entrada: <num_hilos <tamanio>\n");
+		return -1;
+	}
+	
+	thr = atoi(argv[1]);
+	tam = atoi(argv[2]);
+
+	A = generateVector(tam);
+	B = generateVector(tam);
+	if ( !A || !B )
+	{
+		printf("Error when allocationg matrix\n");
+		freeVector(A);
+		freeVector(B);
+		return -1;
+	}
+
+	omp_set_num_threads(thr);	//Fijamos el numero de hilos
+	
+	gettimeofday(&ini,NULL);
+	/* Bloque de computo */
+	sum = 0;
+	#pragma omp parallel for reduction(+:sum)
+	for(k=0;k<tam;k++)
+	{
+		sum = sum + A[k]*B[k];
+	}
+	/* Fin del computo */
+	gettimeofday(&fin,NULL);
+
+	printf("Resultado: %f\n",sum);
+	printf("Tiempo: %f\n", ((fin.tv_sec*1000000+fin.tv_usec)-(ini.tv_sec*1000000+ini.tv_usec))*1.0/1000000.0);
+	freeVector(A);
+	freeVector(B);
+
+	return 0;
+}

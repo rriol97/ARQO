d57d2b574495e39ef7547b6e9009f7492b541bbf
ricardo <ricardo.riol@paco.com>
2017-12-07 13:00:22 +0100

    Paco esta bueno

--- practica4/ej2/script.sh
+++ practica4/ej2/script.sh
@@ -2,8 +2,8 @@
 
 #inicializar variables
 TamIni=50000000
-TamPaso=95000000
-TamFin=1760000000
+TamPaso=49500000
+TamFin=545000000
 Nreps=5
 fDAT=pescalar_tiempos.dat
 fPNG1=pescalar_time.png
@@ -21,9 +21,9 @@ for ((i = 1; i <= Nreps; i++)); do
 	#bucle para variar el numero de hilos
 	for ((j = 1; j <= 4; j++)); do
 		echo "->num_hilos $j/4"
-		for ((k = TamIni; k <= TamFin; k += TamPaso))
-			echo "-> Tamanio $k/TamFin"
-			time_serie=$(./pescalar_serie $j $k |tail -n 1|awk '{print $2}')
+		for ((k = TamIni; k < TamFin; k += TamPaso)); do
+			echo "-> Tamanio $k/$TamFin"
+			time_serie=$(./pescalar_serie $k |tail -n 1|awk '{print $2}')
 			time_par2=$(./pescalar_par2 $j $k |tail -n 1|awk '{print $2}')
 			echo "$j $k $time_serie $time_par2" >> $fDAT
 		done
@@ -39,16 +39,16 @@ awk -v ini=$TamIni -v fin=$TamFin -v paso=$TamPaso -v reps=$Nreps '
 }
 END {
 	for (i = 1; i <= 4; i++){
-		for (x = ini; x <= fin; x += paso) {
-			print "i\tx\t"acum_time_serie[i,x]/reps"\t"acum_time_par[i,x]/reps
+		for (x = ini; x < fin; x += paso) {
+			print i"\t"x"\t"acum_time_serie[i,x]/reps"\t"acum_time_par[i,x]/reps
 		}
 	}	
 }' $fDAT >> pescalar_media.dat
 
-pescalar_media.dat |head -n 10 > pescalar_media_1hilo.dat
-pescalar_media.dat |head -n 20 |tail -n 10 > pescalar_media_2hilos.dat
-pescalar_media.dat |head -n 30 |tail -n 10 > pescalar_media_3hilos.dat
-pescalar_media.dat |tail -n 10 > pescalar_media_4hilos.dat
+cat pescalar_media.dat |head -n 10 >> pescalar_media_1hilo.dat
+cat pescalar_media.dat |head -n 20 |tail -n 10 >> pescalar_media_2hilos.dat
+cat pescalar_media.dat |head -n 30 |tail -n 10 >> pescalar_media_3hilos.dat
+cat pescalar_media.dat |tail -n 10 >> pescalar_media_4hilos.dat
 
 #grafica de tiempos
 gnuplot << END_GNUPLOT
@@ -58,18 +58,18 @@ gnuplot << END_GNUPLOT
 	set key right bottom
 	set grid
 	set term png
-	set output "fPNG1"
-	plot "pescalar_media_1hilo.dat" using 2:3 with lines lw 2 title "1 hilo serie", /
-		 "pescalar_media_1hilo.dat" using 2:4 with lines lw 2 title "1 hilo par", /
-		 "pescalar_media_2hilos.dat" using 2:3 with lines lw 2 title "2 hilos serie", /
-		 "pescalar_media_2hilos.dat" using 2:4 with lines lw 2 title "2 hilos par", /
-		 "pescalar_media_3hilos.dat" using 2:3 with lines lw 2 title "3 hilos serie", /
-		 "pescalar_media_3hilos.dat" using 2:4 with lines lw 2 title "3 hilos par", /
-		 "pescalar_media_4hilos.dat" using 2:3 with lines lw 2 title "4 hilos serie", /
+	set output "$fPNG1"
+	plot "pescalar_media_1hilo.dat" using 2:3 with lines lw 2 title "1 hilo serie", \
+		 "pescalar_media_1hilo.dat" using 2:4 with lines lw 2 title "1 hilo par", \
+		 "pescalar_media_2hilos.dat" using 2:3 with lines lw 2 title "2 hilos serie", \
+		 "pescalar_media_2hilos.dat" using 2:4 with lines lw 2 title "2 hilos par", \
+		 "pescalar_media_3hilos.dat" using 2:3 with lines lw 2 title "3 hilos serie", \
+		 "pescalar_media_3hilos.dat" using 2:4 with lines lw 2 title "3 hilos par", \
+		 "pescalar_media_4hilos.dat" using 2:3 with lines lw 2 title "4 hilos serie", \
 		 "pescalar_media_4hilos.dat" using 2:4 with lines lw 2 title "4 hilos par"
 	replot
 	quit
-END_GNUPLOT	
+END_GNUPLOT
 
 awk -v ini=$TamIni -v fin=$TamFin -v paso=$TamPaso -v reps=$Nreps '
 {
@@ -78,16 +78,16 @@ awk -v ini=$TamIni -v fin=$TamFin -v paso=$TamPaso -v reps=$Nreps '
 }
 END {
 	for (i = 1; i <= 4; i++){
-		for (x = ini; x <= fin; x += paso) {
-			print "i\tx\t"acum_time_serie[i,x]/acum_time_par[i,x]
+		for (x = ini; x < fin; x += paso) {
+			print i"\t"x"\t"acum_time_serie[i,x]/acum_time_par[i,x]
 		}
 	}	
 }' pescalar_media.dat >> pescalar_aceleracion.dat
 
-pescalar_aceleracion.dat |head -n 10 > pescalar_aceleracion_1hilo.dat
-pescalar_aceleracion.dat |head -n 20 |tail -n 10 > pescalar_aceleracion_2hilos.dat
-pescalar_aceleracion.dat |head -n 30 |tail -n 10 > pescalar_aceleracion_3hilos.dat
-pescalar_aceleracion.dat |tail -n 10 > pescalar_aceleracion_4hilos.dat
+cat pescalar_aceleracion.dat |head -n 10 > pescalar_aceleracion_1hilo.dat
+cat pescalar_aceleracion.dat |head -n 20 |tail -n 10 > pescalar_aceleracion_2hilos.dat
+cat pescalar_aceleracion.dat |head -n 30 |tail -n 10 > pescalar_aceleracion_3hilos.dat
+cat pescalar_aceleracion.dat |tail -n 10 > pescalar_aceleracion_4hilos.dat
 
 #grafica de aceleracion
 gnuplot << END_GNUPLOT
@@ -97,11 +97,11 @@ gnuplot << END_GNUPLOT
 	set key right bottom
 	set grid
 	set term png
-	set output "fPNG2"
-	plot "pescalar_aceleracion_1hilo.dat" using 2:3 lines lw 2 title "aceleracion 1 hilo",/
-		 "pescalar_aceleracion_2hilos.dat" using 2:3 lines lw 2 title "aceleracion 2 hilos",/
-		 "pescalar_aceleracion_3hilos.dat" using 2:3 lines lw 2 title "aceleracion 3 hilos",/
-		 "pescalar_aceleracion_4hilos.dat" using 2:3 lines lw 2 title "aceleracion 4 hilos"
+	set output "$fPNG2"
+	plot "pescalar_aceleracion_1hilo.dat" using 2:3 with lines lw 2 title "aceleracion 1 hilo", \
+		 "pescalar_aceleracion_2hilos.dat" using 2:3 with lines lw 2 title "aceleracion 2 hilos", \
+		 "pescalar_aceleracion_3hilos.dat" using 2:3 with lines lw 2 title "aceleracion 3 hilos", \
+		 "pescalar_aceleracion_4hilos.dat" using 2:3 with lines lw 2 title "aceleracion 4 hilos"
 	replot
 	quit
-END_GNUPLOT	
+END_GNUPLOT
